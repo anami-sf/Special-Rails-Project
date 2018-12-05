4e968081431f3801151f64a273e7f6c1a3964c06
Ana <anami823@gmail.com>
2018-12-05 12:37:11 -0800

    Revert "Added controller 'home' and view 'recipes'"
    This reverts commit ff79cea20b1481631738098beddc0ffd06e9343d.

--- test/controllers/home_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get recipies" do
-    get home_recipies_url
-    assert_response :success
-  end
-
-end
