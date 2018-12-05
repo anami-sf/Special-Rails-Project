957808146fe358b61ecc409ed9b666fb82104073
Ana <anami823@gmail.com>
2018-12-05 12:58:01 -0800

    Added 'about' view. Edited root route to home/about. And edited recipes and about routes to bypass '/home'.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/recipes'
+  root 'home#about'
+  get 'recipes', to: 'home#recipes'
+  get 'about', to: 'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
